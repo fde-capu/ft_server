96c363fff8d88a486ab7d6631c242520359f286c
Flavio Carrara De Capua <fde-capu@d3614n.d3614nhost>
2020-04-01 14:08:39 -0300

    Still not private connection!

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-openssl req -new -x509 -nodes -days 365 \
-	-newkey rsa:2048 -passout pass:42sp \
-	-subj "/C=BR/L=SP/O=42SP/CN=ft_server" \
+openssl req -new -x509 -days 365 \
+	-newkey rsa:2048 -nodes \
+	-subj "/C=BR/L=SP/O=42SP/CN=localhost" \
 	-keyout srcs/localhost.key \
 	-out srcs/localhost.crt
-docker container rm -f $(docker ps -aq)
 docker build -t ft_server .
-docker run -d -p 8080:80 -p 443:443 --name ft_container ft_server
+docker run -d -p 8080:80 -p 8081:443 --name ft_container ft_server
