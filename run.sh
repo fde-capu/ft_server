19e57c65c6c5b988886c4dd58ebf1fac154a26ba
Flavio Carrara De Capua <fde-capu@d3614n.d3614nhost>
2020-04-01 18:02:16 -0300

    Autoindex

--- run.sh
+++ run.sh
@@ -11,8 +11,10 @@ openssl req -new -x509 -days 365 \
 docker build -t ft_server .
 
 # Run container
-docker run -d -p 8080:80 -p 443:443 --name ft_container ft_server
+docker run -d -p 8080:80 -p 443:443 \
+	-v "/$(pwd)/srcs/autoindex:/var/www/ft_server/html/autoindex" \
+	--name ft_container ft_server
 
 # Just for verbose sake
-docker ps
 docker logs ft_container
+docker ps
