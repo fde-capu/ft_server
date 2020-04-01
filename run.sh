e21efe33674e4add2a48b548d7cd9ed58ea22155
Flavio Carrara De Capua <fde-capu@d3614n.d3614nhost>
2020-04-01 17:28:14 -0300

    comments

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
+
+# Generate a self signed SSL certificate
 openssl req -new -x509 -days 365 \
 	-newkey rsa:2048 -nodes \
 	-subj "/C=BR/L=SP/O=42SP/CN=localhost" \
 	-keyout srcs/localhost.key \
 	-out srcs/localhost.crt
+
+# Builds image
 docker build -t ft_server .
+
+# Run container
 docker run -d -p 8080:80 -p 8081:443 -p 443:443 --name ft_container ft_server
