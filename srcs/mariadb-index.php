d42562332259b9f49560ec3d0967cd4a5a140f97
Flavio Carrara De Capua <fde-capu@d3614n.d3614nhost>
2020-04-06 17:47:05 -0300

    mariadb filename correction

---
+++ srcs/mariadb-index.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+This is a test for MariaDB.<br>
+<br>
+It is implicit that PHP is running.<br>
+<hr>
+<?php
+
+$link = mysqli_connect('localhost', 'ft_user', 'passwd42sp', 'ft_db');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+printf("MySQL server version: %s\n", mysql_get_server_info());
+?>
+
+?>
+<hr>
+</body>
+</html>
